c5f672e6bab8fe9600c2bf2b8f5fda8de9868f68
rajakushwah <39225004+rajakushwah@users.noreply.github.com>
2021-07-06 16:36:24 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-# import request
 # Create your views here.
+def home(request):
+    param = {'param': "THIS IS SAMPLE APP"}
+    return render(request, 'app/homenew.html', param)
+
+
 def main(request):
+    param = {'param': "THIS IS SAMPLE APP"}
     return render(request, 'app/index.html', param)
