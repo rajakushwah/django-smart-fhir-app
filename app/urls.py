77c4064f0a11cecc5f788c3338633122ef5c2f32
rajakushwah <kushwah.raja1@gmail.com>
2021-07-06 15:35:38 +0530

    main

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path,include
 
 urlpatterns = [
-    path('',views.home,name='home' )
+    path('home',views.home,name='home' ),
+    path('',views.main,name='main' )
 
 ]
--- app/views.py
+++ app/views.py
@@ -3,4 +3,9 @@ import request
 # Create your views here.
 def home(request):
     param = {'param': "THIS IS SAMPLE APP"}
-    return render(request, 'app/home.html', param)
+    return render(request, 'app/homenew.html', param)
+
+
+def main(request):
+    param = {'param': "THIS IS SAMPLE APP"}
+    return render(request, 'app/index.html', param)
